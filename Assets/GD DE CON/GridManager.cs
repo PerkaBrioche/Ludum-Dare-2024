09e1734631b49ac19a9f50aa468e26560069672a
Gabriel <lemergabriel55@gmail.com>
2024-10-07 14:34:43 +0200

    list deco

--- Assets/GD DE CON/GridManager.cs
+++ Assets/GD DE CON/GridManager.cs
@@ -7,6 +7,7 @@ public class GridManager : MonoBehaviour
     [Header("MAP PARAMETERS")]
     public List<EnnemySpawner> List_EnemySpawner;
     public List<TrapSpawner> List_TrapSpawner;
+    public List<TrapSpawner> List_Decooration;
     public PlayerSpawner PlayerSpawner;
     public GameObject ExitDoor;
     public float CameraValue;
